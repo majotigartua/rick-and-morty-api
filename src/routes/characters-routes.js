c5c376b89b7a53b238ed1d01e1a34d68deb6ce9d
María José Torres <majotigartua@gmail.com>
2024-12-26 14:55:27 -0600

    fix code format

--- src/routes/characters-routes.js
+++ src/routes/characters-routes.js
@@ -6,16 +6,19 @@ import { RESPONSE_CODE, RESPONSE_MESSAGE } from "../utils/constants.js";
 const router = Router();
 
 router.get("/characters/alive", async (request, response) => {
-    try {
-        const characters = await getAliveCharacters();
-        if (characters.length === 0) {
-            message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.NOT_FOUND);
-        }
-        message(response, RESPONSE_CODE.OK, null, { results : characters });
+  try {
+    const characters = await getAliveCharacters();
+    if (characters.length === 0) {
+      message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.NOT_FOUND);
     }
-    catch (exception) {
-        message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
-    }
+    message(response, RESPONSE_CODE.OK, null, { results: characters });
+  } catch (exception) {
+    message(
+      response,
+      RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR
+    );
+  }
 });
 
 export default router;
